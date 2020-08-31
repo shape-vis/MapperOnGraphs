6f86d07f92c4464ee1a88377a364a79a6d4571b4
Paul Rosen <prosen@Pauls-MacBook-Pro.local>
2020-08-30 21:27:56 -0400

    major updates to the compuations of filter functions, filtering of mapper graphs, and clustering/community algorithms supported by mapper.

--- data.py
+++ data.py
@@ -3,11 +3,16 @@ import json
 import multiprocessing
 import ntpath
 import os
+import sys
+import time
+
 import networkx as nx
 
 import mog.filter_functions as ff
 
 data_sets = {}
+
+max_time_per_file = 30
 
 filter_function_names = {'agd': 'Average Geodesic Distance',
                          'ecc': 'Eccentricity',
@@ -15,16 +20,35 @@ filter_function_names = {'agd': 'Average Geodesic Distance',
                          'fv': 'Fiedler Vector',
                          'fv_norm': 'Fiedler Vector Normalized',
                          'den_0_5': 'Density 0.5',
-                         'ev_2': 'Eigen Function (2nd)',
-                         'ev_3': 'Eigen Function (3rd)',
-                         'ev_4': 'Eigen Function (4th)',
-                         'ev_5': 'Eigen Function (5th)',
-                         'ev_6': 'Eigen Function (6th)',
-                         'ev_norm_2': 'Eigen Function Normalized (2nd)',
-                         'ev_norm_3': 'Eigen Function Normalized (3rd)',
-                         'ev_norm_4': 'Eigen Function Normalized (4th)',
-                         'ev_norm_5': 'Eigen Function Normalized (5th)',
-                         'ev_norm_6': 'Eigen Function Normalized (6th)'}
+                         'ev_1': 'Eigen Function (2nd)',
+                         'ev_2': 'Eigen Function (3rd)',
+                         'ev_3': 'Eigen Function (4th)',
+                         'ev_4': 'Eigen Function (5th)',
+                         'ev_5': 'Eigen Function (6th)',
+                         'ev_norm_1': 'Eigen Function Normalized (2nd)',
+                         'ev_norm_2': 'Eigen Function Normalized (3rd)',
+                         'ev_norm_3': 'Eigen Function Normalized (4th)',
+                         'ev_norm_4': 'Eigen Function Normalized (5th)',
+                         'ev_norm_5': 'Eigen Function Normalized (6th)'}
+
+'''
+data_gen = ["data/source/not_in_paper/football_out.JSON", "data/source/not_in_paper/airport6632_gcc.JSON",
+            "data/source/not_in_paper/barabasi_albert_graph(10,5).json",
+            "data/source/not_in_paper/barabasi_albert_graph(20,10).json",
+            "data/source/not_in_paper/barabasi_albert_graph(20,3).json",
+            "data/source/not_in_paper/barabasi_albert_graph(50,40).json",
+            "data/source/not_in_paper/bcsstk20.json", "data/source/not_in_paper/bcsstk22.json",
+            "data/source/not_in_paper/beach.json", "data/source/not_in_paper/caveman_graph[3,5].json",
+            "data/source/not_in_paper/caveman_graph[7,4].json", "data/source/not_in_paper/chvatal_graph.json",
+            "data/source/not_in_paper/corr1.json", "data/source/not_in_paper/davis_southern_women_graph.json",
+            "data/source/realworld/circle_of_science.json", "data/source/realworld/USair97.json"]
+'''
+data_gen = []
+for d0 in os.listdir("data/source"):
+    if os.path.isdir("data/source/" + d0):
+        for d1 in os.listdir("data/source/" + d0):
+            if fnmatch.fnmatch(d1.lower(), "*.json"):
+                data_gen.append("data/source/" + d0 + "/" + d1)
 
 
 def read_json_graph(filename):
@@ -38,16 +62,64 @@ def write_json_data(filename, data):
         json.dump(data, outfile)
 
 
+# Generate AGD
+def generate_agd(out_path, graph):
+    if not os.path.exists(out_path):
+        write_json_data(out_path, ff.average_geodesic_distance(graph))
+
+
+# Generate eccentricity
+def generate_ecc(out_path, graph):
+    if not os.path.exists(out_path):
+        write_json_data(out_path, ff.eccentricity(graph))
+
+
+# Generate pagerank
+def generate_pr(out_path, graph, alpha):
+    if not os.path.exists(out_path):
+        write_json_data(out_path, ff.pagerank(graph, alpha))
+
+
+# Generate fiedler vector
+def generate_fv(out_path, graph, weight, normalized):
+    try:
+        if not os.path.exists(out_path):
+            write_json_data(out_path, ff.fiedler_vector(graph, _weight=weight, _normalized=normalized))
+    except nx.exception.NetworkXError:
+        print(">>> FAILED (fiedler vector): graph not connected error")
+
+
+# Generate density
+def generate_den(out_path, graph, eps):
+    if not os.path.exists(out_path):
+        write_json_data(out_path, ff.density(graph, eps))
+
+
+# Generate eigen functions
+def generate_eig(out_path, graph, which_eig, weight, normalized):
+    try:
+        compute = False
+        for ev in which_eig:
+            ev_path = out_path.format(str(ev))
+            if graph.number_of_nodes() > ev and not os.path.exists(ev_path): compute = True
+
+        if compute:
+            eig = ff.eigen_function(graph, _weight=weight, _normalized=normalized)
+            for ev in which_eig:
+                if graph.number_of_nodes() > ev:
+                    ev_path = out_path.format(str(ev))
+                    write_json_data(ev_path, eig[ev][1])
+    except TypeError:
+        print(">>> FAILED (eigen): type error")
+
+
+
 def process_datafile(in_filename):
     print("Processing: " + in_filename)
     data, graph = read_json_graph(in_filename)
 
-    '''
-    cc = list(nx.connected_components(graph))
-    if len(cc) > 1:
-        for _cc in cc:
-            print( len(_cc) )
-    '''
+    gcc = max(nx.connected_components(graph), key=len)
+    graph = graph.subgraph(gcc)
 
     out_filename = "data/"
     if graph.number_of_nodes() < 100:
@@ -56,7 +128,7 @@ def process_datafile(in_filename):
         out_filename += "medium/"
     else:
         out_filename += "large/"
-    out_filename += ntpath.basename(in_filename)
+    out_filename += ntpath.basename(in_filename).lower()
 
     print("                     => " + out_filename)
 
@@ -64,69 +136,44 @@ def process_datafile(in_filename):
     if not os.path.exists(out_dir):
         os.mkdir(out_dir)
 
-    # Generate AGD
-    if not os.path.exists(out_dir + "/agd.json"):
-        write_json_data(out_dir + "/agd.json", ff.average_geodesic_distance(graph))
+    procs = [multiprocessing.Process(target=generate_agd, args=(out_dir + "/agd.json", graph)),
+             multiprocessing.Process(target=generate_ecc, args=(out_dir + "/ecc.json", graph)),
+             multiprocessing.Process(target=generate_pr, args=(out_dir + "/pr_0_85.json", graph, 0.85)),
+             multiprocessing.Process(target=generate_fv, args=(out_dir + "/fv.json", graph, 'value', False)),
+             multiprocessing.Process(target=generate_fv, args=(out_dir + "/fv_norm.json", graph, 'value', True)),
+             multiprocessing.Process(target=generate_den, args=(out_dir + "/den_0_5.json", graph, 0.5)),
+             multiprocessing.Process(target=generate_eig, args=(out_dir + "/ev_{}.json", graph, range(1, 6), 'value', False)),
+             multiprocessing.Process(target=generate_eig, args=(out_dir + "/ev_norm_{}.json", graph, range(1, 6), 'value', True))]
 
-    # Generate eccentricity
-    if not os.path.exists(out_dir + "/ecc.json"):
-        write_json_data(out_dir + "/ecc.json", ff.eccentricity(graph))
+    # process the functions in parallel for max_time_per_file
+    end_time = time.time()+max_time_per_file
+    for p in procs: p.start()
+    for p in procs:
+        p.join(max(1,int(end_time-time.time())))
+        if p.is_alive():
+            p.terminate()
+            p.join()
 
-    # Generate pagerank @ 0.85
-    if not os.path.exists(out_dir + "/pr_0_85.json"):
-        write_json_data(out_dir + "/pr_0_85.json", ff.pagerank(graph, 0.85))
-
-    # Generate fiedler vector
-    if not os.path.exists(out_dir + "/fv.json"):
-        write_json_data(out_dir + "/fv.json", ff.fiedler_vector(graph, _weight='value', _normalized=False))
-
-    # Generate fiedler vector normalized
-    if not os.path.exists(out_dir + "/fv_norm.json"):
-        write_json_data(out_dir + "/fv_norm.json", ff.fiedler_vector(graph, _weight='value', _normalized=True))
-
-    # Generate density
-    if not os.path.exists(out_dir + "/den_0_5.json"):
-        write_json_data(out_dir + "/den_0_5.json", ff.density(graph, 0.5))
-
-    # Generate eigen functions
-    if not os.path.exists(out_dir + "/ev_2.json"):
-        eig = ff.eigen_function(graph, _weight='value', _normalized=False)
-        eig_norm = ff.eigen_function(graph, _weight='value', _normalized=True)
-        for ev in range(2, 6):
-            write_json_data(out_dir + "/ev_" + str(ev) + ".json", eig[ev][1])
-            write_json_data(out_dir + "/ev_norm_" + str(ev) + ".json", eig_norm[ev][1])
-
-    write_json_data(out_filename, data)
+    write_json_data(out_filename, nx.node_link_data(graph))
 
     return data
 
 
-data_gen = ["data/source/not_in_paper/football_out.JSON", "data/source/not_in_paper/airport6632_gcc.JSON",
-            "data/source/not_in_paper/barabasi_albert_graph(10,5).json",
-            "data/source/not_in_paper/barabasi_albert_graph(20,10).json",
-            "data/source/not_in_paper/barabasi_albert_graph(20,3).json",
-            "data/source/not_in_paper/barabasi_albert_graph(50,40).json",
-            "data/source/not_in_paper/bcsstk20.json", "data/source/not_in_paper/bcsstk22.json",
-            "data/source/not_in_paper/beach.json", "data/source/not_in_paper/caveman_graph[3,5].json",
-            "data/source/not_in_paper/caveman_graph[7,4].json", "data/source/not_in_paper/chvatal_graph.json",
-            "data/source/not_in_paper/corr1.json", "data/source/not_in_paper/davis_southern_women_graph.json",
-            "data/source/realworld/circle_of_science.json", "data/source/realworld/USair97.json"]
+if not os.path.exists("data/small"): os.mkdir("data/small")
+if not os.path.exists("data/medium"): os.mkdir("data/medium")
+if not os.path.exists("data/large"): os.mkdir("data/large")
 
 for file in data_gen:
-    p = multiprocessing.Process(target=process_datafile, args=[file])
-    p.start()
-
-    # Wait for 20 seconds or until process finishes
-    p.join(1)
-
-    # If thread is still active
-    if p.is_alive():
-        print("   Taking too long, skipping remaining calculations")
-
-        # Terminate
-        p.terminate()
-        p.join()
-
+    try:
+        process_datafile(file)
+    except json.decoder.JSONDecodeError:
+        print(">>> FAILED: json parse " + file)
+    except TypeError:
+        print(">>> FAILED: type error " + file)
+    except nx.exception.NetworkXError:
+        print(">>> FAILED: graph not connected error " + file)
+    except:
+        print(file + " failed with " + str(sys.exc_info()[0]))
 
 for d0 in os.listdir("data/"):
     if os.path.isdir("data/" + d0):
