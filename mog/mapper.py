6ad08a5786acb16c01c576e5b76b9fa0fc7418c5
Paul Rosen <prosen@Pauls-MacBook-Pro.local>
2020-11-11 17:21:59 -0500

    update to mog calculation, prep for better caching

--- mog/mapper.py
+++ mog/mapper.py
@@ -5,55 +5,45 @@ import networkx as nx
 import statistics as stats
 
 
-# def fill_missing_diffuse_avg(graph, values, missing_vals):
-#     for n in missing_vals:
-#         tot = 0
-#         cnt = 0
-#         for x in graph.neighbors(n):
-#             if x in values:
-#                 tot += values[x]
-#                 cnt += 1
-#
-#         if cnt > 0:
-#             values[n] = tot / cnt
-#
-#
-# def get_min_max(values):
-#     minkey = min(values, key=(lambda k: values[k]))
-#     maxkey = max(values, key=(lambda k: values[k]))
-#     return [values[minkey], values[maxkey]]
+class Cover:
+    def __init__(self, values, intervals, overlap):
+        self.intervals = intervals
+        self.overlap = overlap
+
+        minv = values[ min(values, key=(lambda k: values[k])) ]
+        maxv = values[ max(values, key=(lambda k: values[k])) ]
+
+        per_interval = (maxv - minv) / intervals
+        overlap_amnt = per_interval * overlap
+
+        self.cover = []
+        for x in range(0, intervals):
+            start = minv + per_interval * x - overlap_amnt
+            end = minv + per_interval * (x + 1) + overlap_amnt
+            self.cover.append([start, end])
+
+    def get_cover_elements(self):
+        return self.cover
+
+    def get_intervals(self):
+        return self.intervals
+
+    def get_overlap(self):
+        return self.overlap
 
 
-def form_cover(values, intervals, overlap):
-
-    minv = values[ min(values, key=(lambda k: values[k])) ]
-    maxv = values[ max(values, key=(lambda k: values[k])) ]
-
-    # minmax = get_min_max(values)
-    per_interval = (maxv - minv) / intervals
-    overlap_amnt = per_interval * overlap
-
+def _get_connected_components(graph: nx.classes.graph.Graph, values, cover: Cover):
     ret = []
-    for x in range(0, intervals):
-        start = minv + per_interval * x - overlap_amnt
-        end = minv + per_interval * (x + 1) + overlap_amnt
-        ret.append([start, end])
-
-    return ret
-
-
-def get_connected_components(graph, values, cover):
-    ret = []
-    for ce in cover:
+    for ce in cover.get_cover_elements():
         filtered = list(filter(lambda v: ce[0] <= values[v] <= ce[1], values))
         subg = graph.subgraph(filtered)
         ret += nx.connected_components(subg)
     return ret
 
 
-def get_modularity_components(graph, values, cover):
+def _get_modularity_components(graph: nx.classes.graph.Graph, values, cover: Cover):
     ret = []
-    for ce in cover:
+    for ce in cover.get_cover_elements():
         filtered = list(filter(lambda v: ce[0] <= values[v] <= ce[1], values))
         subg = graph.subgraph(filtered)
         if subg.number_of_edges() == 0:
@@ -63,34 +53,34 @@ def get_modularity_components(graph, values, cover):
     return ret
 
 
-def get_async_label_prop_components(graph, values, cover):
+def _get_async_label_prop_components(graph: nx.classes.graph.Graph, values, cover: Cover):
     ret = []
-    for ce in cover:
+    for ce in cover.get_cover_elements():
         filtered = list(filter(lambda v: ce[0] <= values[v] <= ce[1], values))
         subg = graph.subgraph(filtered)
         ret += list(nx.algorithms.community.asyn_lpa_communities(subg))
     return ret
 
 
-def get_label_prop_components(graph, values, cover):
+def _get_label_prop_components(graph: nx.classes.graph.Graph, values, cover: Cover):
     ret = []
-    for ce in cover:
+    for ce in cover.get_cover_elements():
         filtered = list(filter(lambda v: ce[0] <= values[v] <= ce[1], values))
         subg = graph.subgraph(filtered)
         ret += list(nx.algorithms.community.label_propagation_communities(subg))
     return ret
 
 
-def get_centrality_components(graph, values, cover):
+def _get_centrality_components(graph: nx.classes.graph.Graph, values, cover: Cover):
     ret = []
-    for ce in cover:
+    for ce in cover.get_cover_elements():
         filtered = list(filter(lambda v: ce[0] <= values[v] <= ce[1], values))
         subg = graph.subgraph(filtered)
         ret += list(nx.algorithms.community.girvan_newman(subg))
     return ret
 
 
-def get_nodes(values, components):
+def _get_nodes(values, components):
     nodes = []
     for x in components:
         comp_vals = list(map((lambda _x: values[_x]), x))
@@ -105,7 +95,7 @@ def get_nodes(values, components):
     return nodes
 
 
-def get_links_by_node_overlap(nodes):
+def _get_links_by_node_overlap(nodes):
     links = []
     for i in range(0, len(nodes)):
         node_set_i = set(nodes[i]['components'])
@@ -119,7 +109,7 @@ def get_links_by_node_overlap(nodes):
     return links
 
 
-def get_links_by_edges(input_graph: nx.classes.graph.Graph, mapper_nodes):
+def _get_links_by_graph_cut(input_graph: nx.classes.graph.Graph, mapper_nodes):
     node_map = {}
     for n in input_graph.nodes:
         node_map[n] = []
@@ -141,57 +131,67 @@ def get_links_by_edges(input_graph: nx.classes.graph.Graph, mapper_nodes):
     links = []
     for e in edge_map:
         links.append({"source": e[0], "target": e[1], 'value': edge_map[e]})
-    #     print(str(e) + " " + str(edge_map[e]))
-    # #print(node_map)
-    # for i in range(0, len(mapper_nodes)):
-    #     set_i = set(mapper_nodes[i]['components'])
-    #     for j in range(i + 1, len(mapper_nodes)):
-    #         set_j = set(mapper_nodes[j]['components'])
-    #         w = nx.cut_size( input_graph, set_i, set_j )
-    #         if w > 0:
-    #             if mapper_nodes[i]['min_value'] < mapper_nodes[j]['min_value']:
-    #                 links.append({"source": mapper_nodes[i]['id'], "target": mapper_nodes[j]['id'], 'value': w})
-    #             else:
-    #                 links.append({"target": mapper_nodes[i]['id'], "source": mapper_nodes[j]['id'], 'value': w})
-    #
-    # for l in links:
-    #     print(l)
+
     return links
 
 
 class MapperOnGraphs:
-    def __init__(self, input_graph: nx.classes.graph.Graph, values, cover, component_method='connected_components', link_method='overlap'):
-        self.start_time = time.time()
+    def __init__(self):
+        self.info = {}
 
-        print("A")
+    def load_mog(self, input_file):
+        print("NOT YET IMPLEMENTED")
+
+    def build_mog(self, input_graph: nx.classes.graph.Graph, values, cover: Cover, component_method='connected_components', link_method='overlap', verbose=False):
+        #self.start_time = time.time()
+
+        self.info = {
+            'component_method': component_method,
+            'link_method': link_method,
+            'cover_overlap': cover.get_overlap(),
+            'cover_intervals': cover.get_intervals()
+        }
+
+        start_time = time.time()
+
+        if verbose:
+            print("MapperOnGraphs: Step 1 of 4")
+
         if component_method == 'modularity':
-            self.components = get_modularity_components(input_graph, values, cover)
+            components = _get_modularity_components(input_graph, values, cover)
         elif component_method == 'async_label_prop':
-            self.components = get_async_label_prop_components(input_graph, values, cover)
+            components = _get_async_label_prop_components(input_graph, values, cover)
         elif component_method == 'label_prop':
-            self.components = get_label_prop_components(input_graph, values, cover)
+            components = _get_label_prop_components(input_graph, values, cover)
         elif component_method == 'centrality':
-            self.components = get_centrality_components(input_graph, values, cover)
+            components = _get_centrality_components(input_graph, values, cover)
         else:
-            self.components = get_connected_components(input_graph, values, cover)
+            components = _get_connected_components(input_graph, values, cover)
 
-        print("B")
-        self.nodes = get_nodes(values, self.components)
+        if verbose:
+            print("MapperOnGraphs: Step 2 of 4")
 
-        print("C")
+        nodes = _get_nodes(values, components)
+
+        if verbose:
+            print("MapperOnGraphs: Step 3 of 4")
+
         if link_method == 'connectivity':
-            self.links = get_links_by_edges(input_graph, self.nodes)
+            links = _get_links_by_graph_cut(input_graph, nodes)
         else:
-            self.links = get_links_by_node_overlap(self.nodes)
-        print("D")
+            links = _get_links_by_node_overlap(nodes)
 
-        self.mapper_graph = nx.readwrite.node_link_graph({'nodes': self.nodes, 'links': self.links})
-        print("E")
+        if verbose:
+            print("MapperOnGraphs: Step 4 of 4")
 
-        self.end_time = time.time()
+        self.mapper_graph = nx.readwrite.node_link_graph({'nodes': nodes, 'links': links})
+
+        #self.end_time = time.time()
+        self.info['compute_time'] = time.time()-start_time
 
     def compute_time(self):
-        return self.end_time-self.start_time
+        #return self.end_time-self.start_time
+        return self.info['compute_time']
 
     def number_of_nodes(self):
         return self.mapper_graph.number_of_nodes()
@@ -208,12 +208,15 @@ class MapperOnGraphs:
             del data['components']
 
     def filter_node_size(self, minimum_node_size):
-        node_dict = {}
-        for n in self.nodes:
-            node_dict[n['id']] = n
+        component_sizes = {}
 
-        ns = list(filter((lambda node: len(node_dict[node]['components']) > minimum_node_size), self.mapper_graph.nodes()))
+        for (n,data) in self.mapper_graph.nodes.items():
+            component_sizes[n] = data['component_count']
+
+        ns = list(filter((lambda node: component_sizes[node] > minimum_node_size), self.mapper_graph.nodes()))
         self.mapper_graph = self.mapper_graph.subgraph(ns)
 
     def to_json(self):
-        return json.dumps(nx.node_link_data(self.mapper_graph))
+        json_data = nx.node_link_data(self.mapper_graph)
+        json_data['info'] = self.info
+        return json.dumps(json_data)
