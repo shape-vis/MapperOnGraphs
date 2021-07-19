b8d0b09982a7a860ae62a44f7a0fc682476368c2
Paul Rosen <prosen@usf.edu>
2021-07-19 11:29:38 -0400

    improve code modularity

--- docs/js/graph_loader.js
+++ docs/js/graph_loader.js
@@ -16,6 +16,65 @@ function get_default_mog_options(ds,df,ff,coverN=3,rank=false,component='modular
     }
 }
 
+
+
+let MOG_Vis = function( svg_name, options, static_uri=true ){
+    let param_uri = $.param(options)
+    let mog_data = null
+    let mog_vis = null
+    let uri = 'mog?' + param_uri
+    let ff = options.filter_func
+
+    if( static_uri ){
+        let ds = options.dataset
+        let df = options.datafile
+        df = df.substring(0,df.length-5)
+        let covN = options.coverN
+        let link_m = options.link_method
+        let rank = ( options.rank_filter ) ? 'true' : 'false'
+        let comp_m = options.component_method
+        let overlap = options.coverOverlap
+
+        uri = 'data/' + ds + '/' + df + '/' + ff + '_' + comp_m
+                + '_' + covN + '_' + overlap + '_' + link_m + '_' + rank + '.json'
+    }
+
+    function send_to_cache(){
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "update_mog?"+param_uri, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        tmp_data = {'info':mog_data.info,'nodes':mog_data.nodes,'links':[]};
+        mog_data.links.forEach( function(L){
+           tmp_data.links.push({'value':L.value,'source':L.source.id,'target':L.target.id});
+        });
+        xhr.send(JSON.stringify(tmp_data));
+    }
+
+    d3.json(uri, function (error, data) {
+        if (error) {
+            console.log(error);
+        } else {
+            mog_data = data
+            mog_vis = FDL_Graph_Vis(svg_name, mog_data);
+            mog_vis.set_node_radius(d => Math.min( Math.sqrt(d.comp_len), 20));
+            mog_vis.set_node_color(n=>colorSchemes[ff](n.avg_v));
+            mog_vis.set_link_width(d => Math.min( Math.sqrt(d.value), 20)/2 );
+            mog_vis.add_count_labels();
+            mog_vis.set_end_callback( ()=>{ mog_vis.zoomFit(); send_to_cache() } );
+            mog_vis.load()
+        }
+    });
+
+    return {
+        remove : function(){
+            if( mog_vis ) mog_vis.remove();
+        }
+    }
+
+}
+
+
+
 function __send_graph_to_cache(url, graph_data){
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
@@ -27,30 +86,30 @@ function __send_graph_to_cache(url, graph_data){
     xhr.send(JSON.stringify(tmp_data));
 }
 
-let MOG_Vis = function( svg_name, options ){
-    let param_uri = $.param(options)
-    let mog_data = null
-    let mog_vis = null
-
-    d3.json("mog?" + param_uri, function (error, data) {
-        if (error) {
-            console.log(error);
-        } else {
-            mog_data = data
-            mog_vis = FDL_Graph_Vis(svg_name, mog_data);
-            mog_vis.set_node_radius(d => Math.min( Math.sqrt(d.component_count), 20));
-            mog_vis.set_node_color(n=>colorSchemes[options.filter_func](n.avg_value));
-            mog_vis.set_link_width(d => Math.min( Math.sqrt(d.value), 20)/2 );
-            //mog_vis.add_count_labels();
-            mog_vis.set_end_callback(()=>{
-                mog_vis.zoomFit();
-                __send_graph_to_cache("cache?type=mog_layout&"+param_uri, mog_data)
-            });
-            mog_vis.load()
-        }
-    });
-
-}
+// let MOG_Vis = function( svg_name, options ){
+//     let param_uri = $.param(options)
+//     let mog_data = null
+//     let mog_vis = null
+//
+//     d3.json("mog?" + param_uri, function (error, data) {
+//         if (error) {
+//             console.log(error);
+//         } else {
+//             mog_data = data
+//             mog_vis = FDL_Graph_Vis(svg_name, mog_data);
+//             mog_vis.set_node_radius(d => Math.min( Math.sqrt(d.component_count), 20));
+//             mog_vis.set_node_color(n=>colorSchemes[options.filter_func](n.avg_value));
+//             mog_vis.set_link_width(d => Math.min( Math.sqrt(d.value), 20)/2 );
+//             //mog_vis.add_count_labels();
+//             mog_vis.set_end_callback(()=>{
+//                 mog_vis.zoomFit();
+//                 __send_graph_to_cache("cache?type=mog_layout&"+param_uri, mog_data)
+//             });
+//             mog_vis.load()
+//         }
+//     });
+//
+// }
 
 let Graph_VIS = function(svg_name, options, show_ff = false){
     let param_uri = $.param(options)
