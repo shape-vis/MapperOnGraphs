dab10413d8da157414a9d1968bbba2bfbd66cf9d
Paul Rosen <prosen@usf.edu>
2022-01-21 15:56:30 -0500

    update to setup/run scripts

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
-. venv/bin/activate
+
+if [ "$1" = "clean" ]
+then
+  # delete old directories
+  rm -rf __pycache__
+  rm -rf venv
+fi
+
+if [ -d "venv/" ]
+then
+  # Activate the environment
+  . venv/bin/activate
+else
+  # Create Virtual Environment
+  python3 -m venv venv
+
+  # Activate the environment
+  . venv/bin/activate
+
+  pip install --upgrade pip
+
+  #Within the activated environment, use the following command to install Flask and dependancies:
+  pip install wheel flask
+  #pip install Flask numpy python-dotenv watchdog simplejson blinker waitress gunicorn networkx scipy sklearn
+  pip install -r requirements.txt
+  pip freeze > requirements.txt
+fi
+
+
 
 python analysis.py
 
